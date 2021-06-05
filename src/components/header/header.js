b3f8c8a6e1af3abbcb32423845ccf7f87cc78a8e
Elroy <madcode.el@gmail.com>
2021-06-05 18:06:06 +0800

    update header & app style

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,21 +1,41 @@
 import { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import { Link, useRouteMatch } from 'react-router-dom';
+import styled, { ThemeProvider } from 'styled-components';
 import Container from '../Container/Container';
 import Context from '../../store/index';
+
+function HeaderItem({ label, to, isMatch }) {
+  const match = useRouteMatch({
+    path: to,
+    exact: isMatch,
+  });
+  return (
+    <HeaderContentItem
+      className={match ? '-active' : ''}
+      as={Link}
+      to={to}
+    >{label}</HeaderContentItem>
+  );
+}
 
 function Header() {
   const context = useContext(Context);
   return (
-    <HeaderDom className="header">
-      <Container>
-        <HeaderContent>
-          {
-            Object.keys(context.routes).map((key) => <HeaderContentItem key={`head-item-${key}`}><Link to={context.routes[key].url}>{key}</Link></HeaderContentItem>)
-          }
-        </HeaderContent>
-      </Container>
-    </HeaderDom>
+    <ThemeProvider theme={context.colors}>
+      <HeaderDom className="header">
+        <Container>
+          <HeaderContent>
+            {
+              Object.keys(context.routes).map((key) => <HeaderItem
+                key={`head-item-${key}`}
+                to={context.routes[key].url}
+                label={key}
+              />)
+            }
+          </HeaderContent>
+        </Container>
+      </HeaderDom>
+    </ThemeProvider>
   );
 }
 
@@ -30,16 +50,38 @@ const HeaderDom = styled.header`
 const HeaderContent = styled.div`
   flex: 0 0 auto;
   display: flex;
+  padding-top: 78px;
 `;
 
 const HeaderContentItem = styled.div`
+  position: relative;
   flex: 0 0 auto;
   font-size: 20px;
   font-weight: 400;
   line-height: normal;
   letter-spacing: .47px;
+  padding: 5px 0;
+  color: ${(props) => props.theme.white};
   & + & {
     margin-left: 40px;
+  }
+  &::after {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 2px;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    background-color: ${(props) => props.theme.lightGreen};
+    opacity: 0;
+    transition: opacity 0.2s;
+  }
+  &:hover {
+    color: ${(props) => props.theme.lightGreen}
+  }
+  &.-active::after {
+    opacity: 1;
   }
 `;
 
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,25 +1,33 @@
 import './App.css';
+import { useContext } from 'react';
 import { Route, withRouter } from 'react-router-dom';
+import styled, { ThemeProvider } from 'styled-components';
 import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 import About from '../About/About';
 import Home from '../Home/Home';
 import Products from '../Products/Products';
-// import styled from 'styled-components'
+import Context from '../../store/index';
 
 function App() {
+  const context = useContext(Context);
   return (
-    <div className="App">
-      <Header />
-      <section>
-        <Route path="/home" exact component={Home}></Route>
-        <Route path="/about" exact component={About}></Route>
-        <Route path="/products" exact component={Products}></Route>
-      </section>
-      <footer>
-        footer
-      </footer>
-    </div>
+    <ThemeProvider theme={context.colors}>
+      <AppDom>
+        <Header />
+        <section>
+          <Route path="/home" exact component={Home}></Route>
+          <Route path="/about" exact component={About}></Route>
+          <Route path="/products" exact component={Products}></Route>
+        </section>
+        <Footer />
+      </AppDom>
+    </ThemeProvider>
   );
 }
+
+const AppDom = styled.div`
+  background-color: ${(props) => props.theme.main};
+`;
 
 export default withRouter(App);
