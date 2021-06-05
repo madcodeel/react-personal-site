23725a2f26b42594ea45ca66827a34d3ff500750
Elroy <madcode.el@gmail.com>
2021-06-05 19:22:15 +0800

    add banner to page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,11 +1,14 @@
 import AboutBanner from '../../components/AboutBanner/AboutBanner';
+import Container from '../../components/Container/Container';
 
 function About() {
   return (
     <div className="About">
-      <AboutBanner
-        text="RECOGNIZING THE NEED IS THE PRIMARY CONDITION FOR DESIGN."
-      ></AboutBanner>
+      <Container>
+        <AboutBanner
+          text="RECOGNIZING THE NEED IS THE PRIMARY CONDITION FOR DESIGN."
+        ></AboutBanner>
+      </Container>
     </div>
   );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,16 @@
+import HomeBanner from '../../components/HomeBanner/HomeBanner';
+import Container from '../../components/Container/Container';
+
 function Home() {
   return (
-    <div className="home">Home</div>
+    <div className="home">
+      <Container>
+        <HomeBanner
+          text="WELCOME TO THE PORTFOLIO OF ELROY TSAI, WHO HAS PASSION FOR DIGITAL PRODUCTS."
+          textPadding="20px 50px 200px 30px"
+        ></HomeBanner>
+      </Container>
+    </div>
   );
 }
 
