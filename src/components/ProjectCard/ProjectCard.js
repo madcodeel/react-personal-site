e2e0558c689b4b4285f49329024950ce73b58518
Elroy <madcode.el@gmail.com>
2021-06-05 23:59:53 +0800

    add project list

---
+++ src/components/ProjectCard/ProjectCard.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+function ProjectCard(props) {
+  return (
+    <Wrapper>
+      <ImgBox img={props.img} />
+      <Title>{props.title}</Title>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  position: relative;
+  cursor: pointer;
+`;
+
+const ImgBox = styled.div`
+  position: relative;
+  width: 100%;
+  margin-bottom: 22px;
+  overflow: hidden;
+
+  &::before {
+    content: '';
+    display: block;
+    width: 100%;
+    padding-bottom: 56.25%;
+    background-size: cover;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-color: #000;
+    background-image: url(${(props) => (props.img && props.img !== '' ? props.img : 'https://source.unsplash.com/random/1024x768')});
+    will-change: transform filter;
+    filter: grayscale(0.8);
+    transition: transform 0.5s ease-out, filter 0.5s linear;
+  }
+
+  ${Wrapper}:hover &::before {
+    filter: none;
+    transform: scale(1.1);
+  }
+`;
+
+const Title = styled.p`
+  font-size: 26px;
+  font-weight: 600;
+  line-height: 1.54;
+  letter-spacing: .74px;
+  line-height: 1.3;
+  color: #fff;
+  text-align: center;
+`;
+
+export default ProjectCard;
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,16 +1,51 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
 import ProjectsBanner from '../../components/ProjectsBanner/ProjectsBanner';
 import Container from '../../components/Container/Container';
+import ProjectCard from '../../components/ProjectCard/ProjectCard';
 
 function Projects() {
+  const [projects, setProjects] = useState(null);
+  useEffect(() => {
+    axios.get('/json/projects.json')
+      .then((res) => {
+        console.log(res.data);
+        setProjects(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="Projects">
       <Container>
         <ProjectsBanner
           text="Projects"
         ></ProjectsBanner>
+        {
+          projects && projects.length > 0
+            ? (
+              <ProjectsWrapper>
+                {
+                  projects.map((data) => <ProjectCard img={data.img.thumb} title={data.name} key={`prjoect-${data.name}`} />)
+                }
+              </ProjectsWrapper>
+            )
+            : null
+        }
       </Container>
     </div>
   );
 }
+
+const ProjectsWrapper = styled.div`
+  position: relative;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 32px;
+  margin-bottom: 98px;
+`;
 
 export default Projects;
