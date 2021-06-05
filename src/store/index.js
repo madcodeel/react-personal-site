99664f1ef0ca6dcdec2daa63f22d8b3ff0ed762b
Elroy <madcode.el@gmail.com>
2021-06-05 18:05:51 +0800

    add colors variable

--- src/store/index.js
+++ src/store/index.js
@@ -12,6 +12,12 @@ const Context = React.createContext({
       url: '/products',
     },
   },
+  colors: {
+    main: '#054e62',
+    sub: '#197890',
+    lightGreen: '#b4ff00',
+    white: '#ffffff',
+  },
   loading: false,
   name: 'leo',
 });
